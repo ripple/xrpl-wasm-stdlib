23f11370fd5cc39f449eaceaf46954b0ec8cba75
ricky122-5 <rreddygari@ripple.com>
2026-06-17 16:44:11 -0400

    hash256 done

--- xrpl-wasm-stdlib/xrpl-macros/src/lib.rs
+++ xrpl-wasm-stdlib/xrpl-macros/src/lib.rs
@@ -1,7 +1,13 @@
 use proc_macro::TokenStream;
 mod r_address;
+mod typed_const;
 
 #[proc_macro]
 pub fn r_address(input: TokenStream) -> TokenStream {
     r_address::expand(input)
+}
+
+#[proc_macro]
+pub fn hash256(input: TokenStream) -> TokenStream {
+    typed_const::hash256(input)
 }
