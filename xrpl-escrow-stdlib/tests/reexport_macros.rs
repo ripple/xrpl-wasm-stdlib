a0e8cae5e1db41e5bd03596d05243a31b28c5f4d
ricky122-5 <rreddygari@ripple.com>
2026-07-17 12:54:53 -0400

    fixed example

--- xrpl-escrow-stdlib/tests/reexport_macros.rs
+++ xrpl-escrow-stdlib/tests/reexport_macros.rs
@@ -1,14 +1,7 @@
-#![cfg_attr(target_arch = "wasm32", no_std)]
-
-#[cfg(not(target_arch = "wasm32"))]
-extern crate std;
-
 use xrpl_escrow_stdlib::{EscrowFinishContext, FinishResult};
 use xrpl_macros::smart_escrow;
-use xrpl_wasm_stdlib::host::trace::trace;
 
 #[smart_escrow]
 fn run(_ctx: EscrowFinishContext) -> FinishResult {
-    let _ = trace("Hello World");
     FinishResult::succeed()
 }
