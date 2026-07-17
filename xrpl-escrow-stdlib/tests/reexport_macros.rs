1924495789d3800ca16450cfdecf1152348effb7
ricky122-5 <rreddygari@ripple.com>
2026-07-17 13:33:56 -0400

    removed unused test

--- xrpl-escrow-stdlib/tests/reexport_macros.rs
+++
@@ -1,7 +0,0 @@
-use xrpl_escrow_stdlib::{EscrowFinishContext, FinishResult};
-use xrpl_macros::smart_escrow;
-
-#[smart_escrow]
-fn run(_ctx: EscrowFinishContext) -> FinishResult {
-    FinishResult::succeed()
-}
